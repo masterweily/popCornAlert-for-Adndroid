7eab4d0d2c584645664948ffefa11392cb630d2f
Yaron Weil <masterweily@gmail.com>
2014-03-01 20:32:51 +0200

    added basic inspecting algorithem, popcorn is now ready!

--- src/com/masterweily/PopCornAlert/PopCorningInspector.java
+++ src/com/masterweily/PopCornAlert/PopCorningInspector.java
@@ -1,8 +1,9 @@
 package com.masterweily.PopCornAlert;
 
-import android.os.Handler;
+
 import android.util.Log;
 
+import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -14,10 +15,11 @@ import java.util.TimerTask;
  * To change this template use File | Settings | File Templates.
  */
 public class PopCorningInspector extends Thread {
+    private static final int MIN_POPS = 42;
+    private static final long MAX_POPS_INTERVAL = 3000;
     private final PopCorning controller;
-    private int popsCount;
     private Timer timer;
-
+    private PopList poplist;
 
     // Constructors
 
@@ -32,7 +34,7 @@ public class PopCorningInspector extends Thread {
 
     @Override
     public void run() {
-        popsCount = 0;
+        this.poplist = new PopList();
         timer.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
@@ -41,22 +43,25 @@ public class PopCorningInspector extends Thread {
         },1000L,100L);
     }
 
-    private void inspectPopcornIsReady() {
-        Log.d("inspectPopcorn", "count: " + popsCount + ", ready: " + (isPopcornReady() ? "Yes" : "No"));
-        if (isPopcornReady()) controller.popCornIsReady();
-    }
+    private void inspectPopcornIsReady() { if (isPopcornReady()) controller.popCornIsReady(); }
 
-    private boolean isPopcornReady() { return popsCount > 2; }
+    private boolean isPopcornReady() { return poplist.size() > MIN_POPS && (poplist.lastInterval() > MAX_POPS_INTERVAL || poplist.veryLastInterval() > MAX_POPS_INTERVAL); }
 
     public void close() { timer.cancel(); }
 
-    private void registerPop() { popsCount ++; }
+    private void registerPop() { poplist.registerPop(); }
 
     // Callbacks
 
     public void popDetected() { registerPop(); }
 
+    private class PopList extends ArrayList<Long> {
 
+        public void registerPop(){ poplist.add(new Long(System.currentTimeMillis())); }
 
+        public long lastInterval() { return get(size() - 1) - get(size() - 2); }
+
+        public long veryLastInterval() { return System.currentTimeMillis() - get(size() - 1);  }
+    }
 
 }
