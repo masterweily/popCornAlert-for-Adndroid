96a2a73de58338a9ff29a2f5df226dbcb0faa769
Yaron Weil <masterweily@gmail.com>
2014-03-01 15:56:28 +0200

    detecting pops with sound recorder

--- src/com/masterweily/PopCornAlert/PopCorningDetector.java
+++ src/com/masterweily/PopCornAlert/PopCorningDetector.java
@@ -11,27 +11,33 @@ import java.util.TimerTask;
  * To change this template use File | Settings | File Templates.
  */
 public class PopCorningDetector extends Thread {
+    private static final double POP_AMPLITUDE = 5;
+    private static final long SAMPLE_RATE = 500L;
     private final PopCorning controller;
+    private final SoundRecorder soundRecorder;
     private Timer timer;
 
     public PopCorningDetector(PopCorning controller) {
         super();
         timer = new Timer();
         this.controller = controller;
+        this.soundRecorder = new SoundRecorder();
         this.run();
     }
 
     @Override
     public void run() {
+        this.soundRecorder.run();
         timer.scheduleAtFixedRate(new TimerTask() {
             @Override
-            public void run() {
-                popDetected();
-            }
-        }, 1000L, 1000L);
+            public void run() { if (detectPop()) popDetected(); } }, SAMPLE_RATE, SAMPLE_RATE);
     }
+
+    public boolean detectPop() { return (soundRecorder.getMaxAmplitude() >= POP_AMPLITUDE); }
 
     public void popDetected() { controller.popDetected(); }
 
-    public void close() { timer.cancel(); }
+    public void close() { timer.cancel(); soundRecorder.close(); }
+
+
 }
